16a38c7aa63512baa17fc7c3b7c1c1af3924cb03
Jonathan <jonathan@b-studios.de>
2014-08-05 16:49:03 +0200

    Fixed tests to work with new naming scheme

--- examples/src/test/scala/with.scala
+++ examples/src/test/scala/with.scala
@@ -1,19 +1,19 @@
-package compose
+package composition
 
 object composeMacroTest {
 
   trait A { def a: Int }
   trait B { def b: String }
   trait C { def x: Int }
-  
+
   type D = B with C
-  Compose.compose[A, B with C]
-  Compose.compose[A, D]
-  Compose.compose[A with B, C]
-  implicitly[Compose[A with B, C]]
+  With.compose[A, B with C]
+  With.compose[A, D]
+  With.compose[A with B, C]
+  implicitly[With[A with B, C]]
 
   // now also works with duplicate members:
-  Compose.compose[A, A]
-  Compose.compose[A, B with A]
-  Compose.compose[B, B with C]
+  With.compose[A, A]
+  With.compose[A, B with A]
+  With.compose[B, B with C]
 }
